83e7014d5c7a3742eb88bce0341034f83a45ec62
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-10-08 04:45:37 +0700

    Add Intent for Login/Register button

--- app/src/main/java/com/putragandad/tugasfundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/putragandad/tugasfundamentalandroid/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.putragandad.tugasfundamentalandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin: Button = findViewById(R.id.btn_login)
+        btnLogin.setOnClickListener(this)
+
+        val btnRegister: Button = findViewById(R.id.btn_register)
+        btnRegister.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.btn_login -> {
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.btn_register -> {
+                val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
